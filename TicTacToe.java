8e2271cca001d7463e80e30aeeb873b3868e2dc9
Dem77 <dimitris.tsioumas89@gmail.com>
2014-05-27 16:45:03 +0300

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class TicTacToe {
 
-	public static int[][] tictactoe = { { 1, 1, 0}, { 1, 0, 0 }, { 0, -1, -1 } };
+	public static int[][] tictactoe = { { 1, 1, 1}, { 1, 0, 1 }, { 0, -1, 0 } };
 
 	public static void main(String[] args) {
 
@@ -14,11 +14,11 @@ public class TicTacToe {
 			}
 			System.out.println("");
 		}
-		
-		System.out.println(addLines(tictactoe));
+		System.out.println(add_lines_ver(tictactoe));
+		//System.out.println(addLinesHor(tictactoe));
 	}
 
-	public static int addLines(int[][] test) {
+	public static int addLinesHor(int[][] test) {
 		int result = 0;
 		int winner = -1;
 		for (int i=0; i < test.length; i++) {
@@ -28,6 +28,25 @@ public class TicTacToe {
 				if (result == 3 || result == -3){
 					int line = i + 1;
 					System.out.println("Line " + line + " wins!");
+					winner = 1;
+					break;
+				}
+				if (winner == 1) break;
+			}
+		}
+		return winner;
+	}
+	
+	public static int add_lines_ver (int [] [] test) {
+		int result = 0;
+		int winner = -1;
+		for (int i=0; i < test.length; i++) {
+			result = 0;
+			for(int j=0; j < test[i].length; j++) {
+				result += test[j][i];
+				if (result == 3 || result == -3){
+					int column = i + 1;
+					System.out.println("Column " + column + " wins!");
 					winner = 1;
 					break;
 				}
