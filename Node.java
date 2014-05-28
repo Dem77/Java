771bb6b17b5730e0e3ccf24142443a160cd3a21e
Dem77 <dimitris.tsioumas89@gmail.com>
2014-05-28 13:17:25 +0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,34 @@
+package single;
+
+public class Node {
+	
+	private String element;
+	private Node next;
+	
+	public Node (String s, Node n) {
+		
+		element = s;
+		next = n;
+	}
+
+	public String getElement() {
+		return element;
+	}
+
+	public void setElement(String element) {
+		this.element = element;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [element=" + element + ", next=" + next + "]";
+	}
+}
